0d39d411178ebaeda666fb6bdf557df03f4b3b66
zhengqigo <zhengqigo@vip.163.com>
2019-01-04 12:48:57 +0800

    add method on to post ByteArrayEntity

--- src/main/java/org/fuelteam/watt/httpclient/RequestExecutor.java
+++ src/main/java/org/fuelteam/watt/httpclient/RequestExecutor.java
@@ -10,6 +10,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
 
 import com.alibaba.fastjson.JSON;
@@ -45,6 +46,24 @@ public class RequestExecutor<T extends HttpRequestBase> {
         return this;
     }
 
+    public RequestExecutor<T> on(String uri, Map<String, String> params, Map<String, String> headers,
+            String body) {
+        String url = uri;
+        if (!isNull(params)) url += buildUrlParams(params);
+        t.setURI(URI.create(url));
+        if (!isNull(headers)) {
+            for (String key : headers.keySet()) {
+                t.addHeader(key, headers.get(key));
+            }
+        }
+        if (body == null) return this;
+        if (t instanceof HttpPost) {
+            HttpPost post = (HttpPost) t;
+            post.setEntity(new ByteArrayEntity(body.getBytes()));
+        }
+        return this;
+    }
+    
     public String string(Cookie[] cookies) throws Exception {
         try (CloseableHttpResponse response = RequestClientBuilder.build().get(cookies).execute(t)) {
             return Utf8ResponseHandler.INSTANCE.handleResponse(response);
